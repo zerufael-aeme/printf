4d9f2b91b16e406c9c6aa426329156a33ef2ab6e
Zerufael Aemero <zerufaelaemero@gmail.com>
2023-11-13 12:19:59 -0800

    Header file

---
+++ _printf.c
@@ -0,0 +1,14 @@
+/*
+ * File: _printf.c
+ * Auth: Zerufael Aemero
+ *       Anania Gebretsion
+ */
+
+#include "main.h"
+
+int _printf(const char *format, ...);
+
+int _printf(const char *format, ...)
+{
+
+}
---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <limits.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+
+#endif /*MAIN_H*/
